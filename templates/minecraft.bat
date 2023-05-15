21ff44d55bcfe69fe904d1b752f88325c4b591e8
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2023-05-15 21:58:22 +0200

    templates 	modified:   templates/minecraft.bat

--- templates/minecraft.bat
+++ templates/minecraft.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set java="%~dp0javafolder\bin\javaw.exe"
-set launcher="%~dp0launchername"
-set workingDirectory="%~dp0datadir"
+set java=".\javafolder\bin\javaw.exe"
+set launcher=".\launchername"
+set workingDirectory=".\datadir"
 
 %java% -jar %launcher% --workDir %workingDirectory%
