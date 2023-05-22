b169f2afd9e65b6cba6b60efba61dff86ef0d97a
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2023-05-22 19:49:40 +0200

    testing
    	modified:   portable-minecraft-builder.ps1

--- portable-minecraft-builder.ps1
+++ portable-minecraft-builder.ps1
@@ -13,24 +13,26 @@ $urls = @(
     "https://raw.githubusercontent.com/madkarmaa/portable-minecraft-builder/gui/templates/minecraft.bat",
     "https://raw.githubusercontent.com/madkarmaa/portable-minecraft-builder/gui/templates/Minecraft.vbs",
     "https://raw.githubusercontent.com/madkarmaa/portable-minecraft-builder/gui/utils/launcher-downloader.ps1",
-    "https://raw.githubusercontent.com/madkarmaa/portable-minecraft-builder/gui/utils/java-downloader.ps1"
+    "https://raw.githubusercontent.com/madkarmaa/portable-minecraft-builder/gui/utils/java-downloader.ps1",
+    "https://raw.githubusercontent.com/madkarmaa/portable-minecraft-builder/gui/utils/fabric-downloader.ps1",
+    "https://raw.githubusercontent.com/madkarmaa/portable-minecraft-builder/gui/utils/mods-downloader.ps1"
 )
 
 foreach ($url in $urls) {
     & ".\file-downloader.ps1" -Url $url
 }
 
-& ".\utils\launcher-downloader.ps1"
-& ".\utils\java-downloader.ps1"
+& ".\launcher-downloader.ps1"
+& ".\java-downloader.ps1"
 
 if ($InstallFabric) {
-    & ".\utils\fabric-downloader.ps1"
+    & ".\fabric-downloader.ps1"
 
     if ($InstallMods) {
         $projectNames = @("fabric-api", "iris", "lithium", "sodium", "starlight")
 
         foreach ($projectName in $projectNames) {
-            & ".\utils\mods-downloader.ps1" -projectName $projectName
+            & ".\mods-downloader.ps1" -projectName $projectName
         }
     }
 }
