e419e65a7ca051efacf870708fe565a3b6ab0521
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2023-05-22 21:12:29 +0200

    testing
    	modified:   gui.ps1
    	modified:   portable-minecraft-builder.ps1

--- portable-minecraft-builder.ps1
+++ portable-minecraft-builder.ps1
@@ -21,27 +21,27 @@ $urls = @(
 $javaPath = ".\Java\bin\javaw.exe"
 
 foreach ($url in $urls) {
-    Start-Process powershell.exe -ArgumentList "-File", ".\file-downloader.ps1", "-Url", $url -NoNewWindow
+    Start-Process powershell.exe -ArgumentList "-File", '".\file-downloader.ps1"', "-Url", $url -NoNewWindow
 }
 
-Start-Process powershell.exe -ArgumentList "-File", ".\launcher-downloader.ps1" -NoNewWindow
-Start-Process powershell.exe -ArgumentList "-File", ".\java-downloader.ps1" -NoNewWindow
+Start-Process powershell.exe -ArgumentList "-File", '".\launcher-downloader.ps1"' -NoNewWindow
+Start-Process powershell.exe -ArgumentList "-File", '".\java-downloader.ps1"' -NoNewWindow
 
 if ($InstallFabric) {
-    Start-Process powershell.exe -ArgumentList "-File", ".\fabric-downloader.ps1" -NoNewWindow
+    Start-Process powershell.exe -ArgumentList "-File", '".\fabric-downloader.ps1"' -NoNewWindow
     $fileExists = Test-Path -Path ".\$DataFolderName\launcher_profiles.json"
 
     if (-not ($fileExists)) {
-        Start-Process -FilePath $javaPath -ArgumentList "-jar", ".\SKlauncher.jar", "--workDir", $DataFolderName -NoNewWindow -Wait
+        Start-Process -FilePath $javaPath -ArgumentList "-jar", '".\SKlauncher.jar"', "--workDir", $DataFolderName -NoNewWindow -Wait
     }
 
-    Start-Process -FilePath $javaPath -ArgumentList "-jar", ".\fabric.jar", "client", "-dir", $DataFolderName -NoNewWindow
+    Start-Process -FilePath $javaPath -ArgumentList "-jar", '".\fabric.jar"', "client", "-dir", $DataFolderName -NoNewWindow
 
     if ($InstallMods) {
         $projectNames = @("fabric-api", "iris", "lithium", "sodium", "starlight")
 
         foreach ($projectName in $projectNames) {
-            Start-Process powershell.exe -ArgumentList "-File", ".\mods-downloader.ps1", "-projectName", $projectName, "-DataFolderName", $DataFolderName -NoNewWindow
+            Start-Process powershell.exe -ArgumentList "-File", '".\mods-downloader.ps1"', "-projectName", $projectName, "-DataFolderName", $DataFolderName -NoNewWindow
         }
     }
 }
