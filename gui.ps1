e59e65e8b1ec0a68d6b58accb68e144ed63ba4ec
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2023-05-24 18:26:50 +0200

    New UI style
    TODO:
     - Style the UI - DONE
     - Let the window move
     - Minimize the terminal window - REJECTED
     - (maybe) Try-Except blocks - DONE
     - Batch launcher - DONE
     - Auto-delete gui.ps1 - DONE
    
    	modified:   gui.ps1

--- gui.ps1
+++ gui.ps1
@@ -27,25 +27,43 @@ Add-Type -AssemblyName System.Windows.Forms
 
 $folderPath = ".\Java"
 
+# Color palette
+$accentColor1 = [System.Drawing.Color]::FromArgb(25, 23, 37)
+# $accentColor2 = [System.Drawing.Color]::FromArgb(89, 81, 140)
+$accentColor3 = [System.Drawing.Color]::FromArgb(117, 108, 191)
+$accentColor4 = [System.Drawing.Color]::FromArgb(202, 172, 242)
+$accentColor5 = [System.Drawing.Color]::FromArgb(241, 195, 242)
+$successColor = [System.Drawing.Color]::FromArgb(124, 252, 0)
+
 # Create the form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Portable Minecraft Builder (win64)"
 $form.Size = New-Object System.Drawing.Size(800, 500)
 $form.StartPosition = "CenterScreen"
-$form.BackColor = [System.Drawing.Color]::FromArgb(53, 55, 64)
+$form.BackColor = $accentColor1
 $form.ForeColor = [System.Drawing.Color]::White
 
-# Set the accent color to purple
-$accentColor = [System.Drawing.Color]::FromArgb(155, 89, 182)
-$successColor = [System.Drawing.Color]::FromArgb(124, 252, 0)
+# Create the label for the title
+$title = New-Object System.Windows.Forms.Label
+$title.Size = New-Object System.Drawing.Size(410, 40)
+$title.Anchor = [System.Windows.Forms.AnchorStyles]::None
+$title.Left = ($form.ClientSize.Width - $title.Width) / 2
+$title.Top = 10
+$title.Text = "Portable Minecraft Builder"
+$title.ForeColor = $accentColor3
+$title.Font = "Lucida Console Regular,20"
+$form.Controls.Add($title)
 
-# Create the label for SKlauncher and JDK 17
+# Create the label for attributions
 $labelTop = New-Object System.Windows.Forms.Label
-$labelTop.Location = New-Object System.Drawing.Point(10, 10)
+$labelTop.Size = New-Object System.Drawing.Size(390, 20)
+$labelTop.Anchor = [System.Windows.Forms.AnchorStyles]::None
+$labelTop.Left = ($form.ClientSize.Width - $labelTop.Width) / 2
+$labelTop.Top = 55
 $labelTop.AutoSize = $true
-$labelTop.Text = "Using SKlauncher and JDK 17 (Temurin 17 LTS). Visit https://skmedix.pl/ and https://adoptium.net/"
-$labelTop.ForeColor = $accentColor
-# $labelTop.Font = "Comic Sans,20"
+$labelTop.Text = "Using SKlauncher and JDK 17 (Temurin 17 LTS)."
+$labelTop.ForeColor = $accentColor4
+$labelTop.Font = "Lucida Console Regular,10, style=Underline"
 $form.Controls.Add($labelTop)
 
 # Check if the "jdk" folder exists in the current directory
@@ -53,16 +71,17 @@ $jdkFolderExists = Test-Path -Path $folderPath -PathType Container
 
 # Create a label for the "Delete Java folder" switch
 $labelDeleteJdk = New-Object System.Windows.Forms.Label
-$labelDeleteJdk.Location = New-Object System.Drawing.Point(10, 170)
-$labelDeleteJdk.Size = New-Object System.Drawing.Size(200, 30)
+$labelDeleteJdk.Location = New-Object System.Drawing.Point(10, 220)
+$labelDeleteJdk.Size = New-Object System.Drawing.Size(210, 50)
 $labelDeleteJdk.Text = "Delete pre-existing Java folder?"
-$labelDeleteJdk.ForeColor = $accentColor
+$labelDeleteJdk.Font = "Lucida Console Regular,10"
+$labelDeleteJdk.ForeColor = $accentColor5
 $form.Controls.Add($labelDeleteJdk)
 
 # Create a switch for the "Delete Java folder" option
 $switchDeleteJdk = New-Object System.Windows.Forms.CheckBox
-$switchDeleteJdk.Location = New-Object System.Drawing.Point(210, 165)
-$switchDeleteJdk.Size = New-Object System.Drawing.Size(20, 30)
+$switchDeleteJdk.Location = New-Object System.Drawing.Point(230, 217)
+$switchDeleteJdk.Size = New-Object System.Drawing.Size(30, 30)
 $switchDeleteJdk.Add_CheckStateChanged({
     Write-Host "Delete Java:" $switchDeleteJdk.Checked
 })
@@ -70,16 +89,17 @@ $form.Controls.Add($switchDeleteJdk)
 
 # Create a label for the "Delete data folder" switch
 $labelDeleteData = New-Object System.Windows.Forms.Label
-$labelDeleteData.Location = New-Object System.Drawing.Point(10, 210)
-$labelDeleteData.Size = New-Object System.Drawing.Size(200, 30)
+$labelDeleteData.Location = New-Object System.Drawing.Point(10, 270)
+$labelDeleteData.Size = New-Object System.Drawing.Size(210, 50)
 $labelDeleteData.Text = "Delete pre-existing data folder?"
-$labelDeleteData.ForeColor = $accentColor
+$labelDeleteData.Font = "Lucida Console Regular,10"
+$labelDeleteData.ForeColor = $accentColor5
 $form.Controls.Add($labelDeleteData)
 
 # Create a switch for the "Delete data folder" option
 $switchDeleteData = New-Object System.Windows.Forms.CheckBox
-$switchDeleteData.Location = New-Object System.Drawing.Point(210, 205)
-$switchDeleteData.Size = New-Object System.Drawing.Size(20, 30)
+$switchDeleteData.Location = New-Object System.Drawing.Point(230, 267)
+$switchDeleteData.Size = New-Object System.Drawing.Size(30, 30)
 $switchDeleteData.Add_CheckStateChanged({
     Write-Host "Delete data folder:" $switchDeleteData.Checked
 })
@@ -105,19 +125,20 @@ if ($jdkFolderExists) {
 
 # Create a label for the custom data folder name input
 $labelFolder = New-Object System.Windows.Forms.Label
-$labelFolder.Location = New-Object System.Drawing.Point(10, 50)
-$labelFolder.Size = New-Object System.Drawing.Size(200, 30)
+$labelFolder.Location = New-Object System.Drawing.Point(10, 100)
+$labelFolder.Size = New-Object System.Drawing.Size(210, 30)
 $labelFolder.Text = "Custom data folder name:"
-$labelFolder.ForeColor = $accentColor
+$labelFolder.Font = "Lucida Console Regular,10"
+$labelFolder.ForeColor = $accentColor5
 $form.Controls.Add($labelFolder)
 
 # Create a text input for the custom data folder name
 $textFolder = New-Object System.Windows.Forms.TextBox
-$textFolder.Location = New-Object System.Drawing.Point(210, 45)
+$textFolder.Location = New-Object System.Drawing.Point(230, 100)
 $textFolder.Size = New-Object System.Drawing.Size(250, 30)
 $textFolder.Text = ".minecraft"
-$textFolder.BackColor = [System.Drawing.Color]::FromArgb(50, 50, 50)
-$textFolder.ForeColor = [System.Drawing.Color]::White
+$textFolder.BackColor = [System.Drawing.Color]::White
+$textFolder.ForeColor = [System.Drawing.Color]::Black
 $textFolder.Add_TextChanged({
     $newText = $textFolder.Text
     $newText = $newText -replace '\s', '-'
@@ -143,16 +164,17 @@ $form.Controls.Add($textFolder)
 
 # Create a label for the "Install Fabric" switch
 $labelFabric = New-Object System.Windows.Forms.Label
-$labelFabric.Location = New-Object System.Drawing.Point(10, 90)
-$labelFabric.Size = New-Object System.Drawing.Size(200, 30)
+$labelFabric.Location = New-Object System.Drawing.Point(10, 140)
+$labelFabric.Size = New-Object System.Drawing.Size(210, 30)
 $labelFabric.Text = "Install Fabric:"
-$labelFabric.ForeColor = $accentColor
+$labelFabric.Font = "Lucida Console Regular,10"
+$labelFabric.ForeColor = $accentColor5
 $form.Controls.Add($labelFabric)
 
 # Create a switch for the "Install Fabric" option
 $switchFabric = New-Object System.Windows.Forms.CheckBox
-$switchFabric.Location = New-Object System.Drawing.Point(210, 85)
-$switchFabric.Size = New-Object System.Drawing.Size(20, 30)
+$switchFabric.Location = New-Object System.Drawing.Point(230, 137)
+$switchFabric.Size = New-Object System.Drawing.Size(30, 30)
 $switchFabric.Add_CheckStateChanged({
     Write-Host "Install Fabric:" $switchFabric.Checked
     if ($switchFabric.Checked) {
@@ -167,17 +189,18 @@ $form.Controls.Add($switchFabric)
 
 # Create a label for the "Install optimization mods" switch
 $labelMods = New-Object System.Windows.Forms.Label
-$labelMods.Location = New-Object System.Drawing.Point(10, 130)
-$labelMods.Size = New-Object System.Drawing.Size(200, 30)
+$labelMods.Location = New-Object System.Drawing.Point(10, 180)
+$labelMods.Size = New-Object System.Drawing.Size(210, 30)
 $labelMods.Text = "Install optimization mods:"
-$labelMods.ForeColor = $accentColor
+$labelMods.Font = "Lucida Console Regular,10"
+$labelMods.ForeColor = $accentColor5
 $labelMods.Visible = $false
 $form.Controls.Add($labelMods)
 
 # Create a switch for the "Install optimization mods" option
 $switchMods = New-Object System.Windows.Forms.CheckBox
-$switchMods.Location = New-Object System.Drawing.Point(210, 125)
-$switchMods.Size = New-Object System.Drawing.Size(20, 30)
+$switchMods.Location = New-Object System.Drawing.Point(230, 177)
+$switchMods.Size = New-Object System.Drawing.Size(30, 30)
 $switchMods.Visible = $false
 $switchMods.Add_CheckStateChanged({
     Write-Host "Install optimization mods:" $switchMods.Checked
@@ -191,7 +214,7 @@ $buttonInstall.Text = "Install"
 $buttonInstall.Anchor = [System.Windows.Forms.AnchorStyles]::None
 $buttonInstall.Left = ($form.ClientSize.Width - $buttonInstall.Width) / 2
 $buttonInstall.Top = $form.ClientSize.Height - $buttonInstall.Height - 50
-$buttonInstall.BackColor = $accentColor
+$buttonInstall.BackColor = $accentColor3
 $buttonInstall.ForeColor = [System.Drawing.Color]::White
 $buttonInstall.Add_Click({
     Write-Host "`n[35m========== INSTALLATION START ==========[0m`n"
