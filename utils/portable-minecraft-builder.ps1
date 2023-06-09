4da03a45ff528e202eec67d007547cb72c0d0377
madkarmaa <madkarma99@gmail.com>
2023-06-09 12:41:34 +0200

    Fixed renaming the launcher file
    	modified:   utils/portable-minecraft-builder.ps1

--- utils/portable-minecraft-builder.ps1
+++ utils/portable-minecraft-builder.ps1
@@ -51,6 +51,12 @@ try {
     Log "Downloading launcher..."
     
     Start-Process powershell.exe -ArgumentList "-Command", '".\file-downloader.ps1"', "-Url", "https://skmedix.pl//data/SKlauncher-3.1.jar" -NoNewWindow -Wait
+    $launcherFile = Get-ChildItem -Path "C:\Path\to\directory" -Filter "sklauncher*.jar" -Recurse | Where-Object { $_.Name -like "sklauncher*.jar" -and $_.Name -cne "SKlauncher.jar" }
+
+    if ($launcherFile) {
+        Rename-Item -Path $launcherFile.FullName -NewName "SKlauncher.jar" -Force
+    }
+
     Remove-Item -Path ".\launcher-downloader.ps1" -Force
     Remove-Item -Path ".\file-downloader.ps1" -Force
     
